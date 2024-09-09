ad94a7f26b78fe51e0b3ae6937bfa292676f40e6
Barreto <v.barreto.and@gmail.com>
2024-09-09 02:55:45 -0300

    add open feign configuration

--- src/main/java/com/e3gsix/fiap/tech_challenge_5_payment/TechChallenge5PaymentApplication.java
+++ src/main/java/com/e3gsix/fiap/tech_challenge_5_payment/TechChallenge5PaymentApplication.java
@@ -2,8 +2,10 @@ package com.e3gsix.fiap.tech_challenge_5_payment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class TechChallenge5PaymentApplication {
 
 	public static void main(String[] args) {
