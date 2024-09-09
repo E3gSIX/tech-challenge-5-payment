379a29df49b68119a14f3101f726d84fd009622a
Barreto <v.barreto.and@gmail.com>
2024-09-09 03:05:45 -0300

    fix enumerated annotations in right attributes

--- src/main/java/com/e3gsix/fiap/tech_challenge_5_payment/model/entity/Payment.java
+++ src/main/java/com/e3gsix/fiap/tech_challenge_5_payment/model/entity/Payment.java
@@ -1,5 +1,7 @@
 package com.e3gsix.fiap.tech_challenge_5_payment.model.entity;
 
+import com.e3gsix.fiap.tech_challenge_5_payment.model.enums.PaymentStatus;
+import com.e3gsix.fiap.tech_challenge_5_payment.model.enums.PaymentType;
 import jakarta.persistence.*;
 
 import java.time.LocalDateTime;
@@ -12,11 +14,29 @@ public class Payment {
 
     String username;
     Long shoppingCartId;
+
+    @Enumerated(EnumType.STRING)
     PaymentType type;
 
     @Enumerated(EnumType.STRING)
     PaymentStatus status;
 
-    @Enumerated(EnumType.STRING)
     LocalDateTime lastUpdate;
+
+    public Payment() {
+    }
+
+    public Payment(
+            String username,
+            Long shoppingCartId,
+            PaymentType type,
+            PaymentStatus status,
+            LocalDateTime lastUpdate
+    ) {
+        this.username = username;
+        this.shoppingCartId = shoppingCartId;
+        this.type = type;
+        this.status = status;
+        this.lastUpdate = lastUpdate;
+    }
 }
