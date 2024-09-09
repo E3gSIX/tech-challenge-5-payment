a51851bf7580eb46a69040a9471dc0d6dd492ce8
Barreto <v.barreto.and@gmail.com>
2024-09-08 23:43:22 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Tech Challenge 5 - Payment
+Microsserviço designado ao pagamento de carrinhos de um e-commerce.
---
+++ src/main/java/com/e3gsix/fiap/tech_challenge_5_payment/TechChallenge5PaymentApplication.java
@@ -0,0 +1,13 @@
+package com.e3gsix.fiap.tech_challenge_5_payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechChallenge5PaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechChallenge5PaymentApplication.class, args);
+	}
+
+}
